84032cb7939965db397e3601aee256ba5ea984b5
Grumlen <grumlen@gmail.com>
2025-09-26 14:55:56 -0500

    circpleshape

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while 1 < 2:
         screen.fill("black")
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
